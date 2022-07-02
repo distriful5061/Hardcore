b5553db55323deb3dc8189ec1ec2e054c721e257
rm809 <83632267+distriful5061@users.noreply.github.com>
2022-07-03 06:54:01 +0900

    a

---
+++ Hardcore/src/main/java/com/github/distriful5061/hardcore/hardcoreplug/HardcorePlug.java
@@ -0,0 +1,17 @@
+package com.github.distriful5061.hardcore.hardcoreplug;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcorePlug extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
